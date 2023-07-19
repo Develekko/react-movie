7bf67fc6760c1f8446f8856763f7a925b96ad8a1
Housam Ramadan <Housamsoma99@gmail.com>
2023-07-19 13:07:10 +0300

    update

--- src/utlis/socket.js
+++ src/utlis/socket.js
@@ -1,11 +1,11 @@
 import io from 'socket.io-client';
 
-const baseURL = 'http://localhost:5000'
-// export const clientIo = io('https://ecommerce.elafglass.com',{
-//     transports: ["websocket", "polling"],
-//     withCredentials: true,
-//   })
-export const clientIo = io(baseURL,{
-  transports: ["websocket", "polling"],
-  withCredentials: true,
-})
+//const baseURL = 'http://localhost:5000'
+export const clientIo = io('https://ecommerce.elafglass.com',{
+    transports: ["websocket", "polling"],
+    withCredentials: true,
+  })
+// export const clientIo = io(baseURL,{
+//   transports: ["websocket", "polling"],
+//   withCredentials: true,
+// })
